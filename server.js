af1d477289561d48ea63931e6b7d2e8926339496
Renan Torres <rgrtorres@hotmail.com>
2025-05-07 21:57:47 -0300

    Publicação do projeto

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.static('public'))
+
+
+// Lidar com conexões Socket.io
+io.on('connection', (socket) => {
+  console.log('Novo cliente conectado', socket.id);
+
+  socket.on('sendNotification', (data) => {
+    console.log('notificação enviada')
+    io.emit('receiveNotification', data)
+  })
+
+  socket.on('disconnect', (reason) => {
+    console.log('Cliente desconectado:', socket.id, 'Motivo:', reason);
+  });
+})
+
+const PORT = 4545
+server.listen(PORT, () => {
+  console.log('Server running')
+})
